157a5794b4a6b08fbf2db176d23326bc0febbf37
KULBHUSHAN <krishnathakur0007895@gmail.com>
2023-12-25 12:11:58 +0530

    Fixed navbar issue

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,34 +1,36 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 import PropTypes from 'prop-types'
 
+
 export class Navbar extends Component {
-    static propTypes = {
-
-    }
-
-    render() {
-        return (
-            <div>
-                <nav className="navbar navbar-expand-lg bg-light">
-                    <div className="container-fluid">
-                        <a className="navbar-brand" href="\">Navbar</a>
-                        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                            <span className="navbar-toggler-icon"></span>
-                        </button>
-                        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                                <li className="nav-item">
-                                    <a className="nav-link active" aria-current="page" href="\">Home</a>
-                                </li>
-                                <li className="nav-item">
-                                    <a className="nav-link" href="\">Link</a>
-                                </li> 
-                            </ul>
-                        </div>
-                    </div>
-                </nav>
+  
+  render() {
+    return (
+      <div>
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark" style={{fontFamily:"sans-serif", display:"flex", alignContent:"center", alignItems:"center", fontSize:"15px", position:"sticky", top:"0", zIndex:"100"}}>
+          <div className="container-fluid text-lg ">
+            <a className="navbar-brand ms-auto" style={{ display:"flex", alignContent:"center", alignItems:"center", fontSize:"18px"}} href="/">NewsMonkey</a>
+            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+              <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                <li className="nav-item ">
+                  <a className="nav-link active" aria-current="page" href="https://news-web-app-git-main-mr-kbrkd.vercel.app/">Home</a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link active " href="https://github.com/mr-kbRKD/News-Web-App">Link</a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link active " href="https://my-portfolio-kbrkd.vercel.app/">Contact Us</a>
+                </li>
+              </ul>
             </div>
-        )
-        }
-    }
-        export default Navbar
+          </div>
+        </nav>
+      </div>
+    );
+  }
+}
+
+export default Navbar;
